917f71766c35efd2972febf75cd9ed8568303a19
梁荣振 <liangrongzhen@yizumi.com>
2026-02-26 09:52:37 +0800

    properties

--- jmaa-server/src/main/java/org/jmaa/server/ApplicationStartup.java
+++ jmaa-server/src/main/java/org/jmaa/server/ApplicationStartup.java
@@ -4,17 +4,22 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jmaa.sdk.exceptions.ModelException;
 import org.jmaa.sdk.tenants.Tenant;
 import org.jmaa.sdk.tenants.TenantService;
 
+import org.jmaa.sdk.tools.DigestUtils;
 import org.jmaa.sdk.tools.PathUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.stereotype.Component;
+
+import static jmaa.modules.tenant.models.TenantDataSource.CODE_FORMAT_PATTERN;
 
 /**
  * web启动事件
@@ -23,7 +28,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ApplicationStartup implements ApplicationListener<ApplicationReadyEvent> {
-
+    static final Pattern CODE_FORMAT_PATTERN = Pattern.compile("^<code\\|(.+?)>$");
     @Value("${spring.profiles.active}")
     private String activeEnv;
 
@@ -32,17 +37,28 @@ public class ApplicationStartup implements ApplicationListener<ApplicationReadyE
         String env = this.activeEnv == null ? "dev" : this.activeEnv;
         String propertiesFileName = "dbcp-" + env + ".properties";
         Properties properties = find(PathUtils.combine("config", propertiesFileName));
-        if(properties == null){
+        if (properties == null) {
             properties = find(propertiesFileName);
         }
-        if(properties == null){
+        if (properties == null) {
             try {
                 properties = PropertiesLoaderUtils.loadAllProperties(propertiesFileName);
             } catch (Exception e) {
                 throw new ModelException("load dbcp error", e);
             }
         }
+        decode(properties, "username");
+        decode(properties, "password");
         TenantService.register(new Tenant("root", "Root", properties));
+    }
+
+    static void decode(Properties properties, String key) {
+        String code = properties.getProperty(key);
+        Matcher codeMatcher = CODE_FORMAT_PATTERN.matcher(code);
+        if (codeMatcher.matches()) {
+            String decode = DigestUtils.DES.decode(codeMatcher.group(1));
+            properties.setProperty(key, decode);
+        }
     }
 
     static Properties find(String propertiesFileName) {

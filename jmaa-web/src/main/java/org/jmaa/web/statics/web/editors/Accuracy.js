a1a471cb510c2b53a2ca373a725eeaaf4e94359d
梁荣振 <liangrongzhen@yizumi.com>
2026-02-26 11:12:44 +0800

    accuracy

--- jmaa-web/src/main/java/org/jmaa/web/statics/web/editors/Accuracy.js
+++ jmaa-web/src/main/java/org/jmaa/web/statics/web/editors/Accuracy.js
@@ -2,6 +2,7 @@ jmaa.editor('accuracy', {
     extends: 'editors.float',
     init() {
         let me = this;
+        me.decimals = 10;
         me.callSuper();
         let field = me.dom.attr('accuracy');
         me.owner.editors[field].onValueChange(function () {
